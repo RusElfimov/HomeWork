f310069e5c80f42d67b6c3ba18dc7bd137889fbc
RusElfimov <ruselfimovbox@outlook.com>
2022-10-03 19:28:00 +0300

    save

---
+++ HomeWork002/Program.cs
@@ -0,0 +1,8 @@
+﻿ Console.Write("Введите целое число ");//четное не четное
+       int x = int.Parse(Console.ReadLine());
+       double x_2 = x % 2;
+       if (x_2==0) 
+          Console.WriteLine("число четное");
+       else
+       Console.WriteLine("Число нечетное");
+      
